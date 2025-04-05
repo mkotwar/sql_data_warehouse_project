23a54fda60370cda0357f12526cc812c9ebb7cc0
Mukul Kotwar <119514678+mkotwar@users.noreply.github.com>
2025-04-05 22:38:34 +0100

    Update ddl_bronze.sql

--- script/bronze/ddl_bronze.sql
+++ script/bronze/ddl_bronze.sql
@@ -1,3 +1,16 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+
+
 use DataWarehouse;
 
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
